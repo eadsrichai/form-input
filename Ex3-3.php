f7dcf5f5b12476a7b4d3f0c29d20bcfb4f6207f2
Wuttiwong <wutkmutt@hotmail.com>
2024-06-29 11:56:07 +0700

    Secondary commit

--- Ex3-3.php
+++ Ex3-3.php
@@ -29,8 +29,11 @@
                 <div class="col-1">
                    
                 </div>
-                <div class="col-2">
+                <div class="col-4">
                     <input type="submit" class="btn btn-primary" name="submit" value="บวก"/>
+                    <input type="submit" class="btn btn-success" name="submit" value="ลบ"/>
+                    <input type="submit" class="btn btn-warning" name="submit" value="คูณ"/>
+                    <input type="submit" class="btn btn-secondary" name="submit" value="หาร"/>
                 </div>
             </div>
         </form>
@@ -39,6 +42,12 @@
     if(isset($_GET['submit'])){
         $n1 = $_GET['number1'];
         $n2 = $_GET['number2'];
+        $result = 0.00;
+        if($_GET['submit'] == 'บวก'){       $result = $n1 + $n2;
+        }else if($_GET['submit'] == 'ลบ'){  $result = $n1 - $n2;
+        }else if($_GET['submit'] == 'คูณ'){  $result = $n1 * $n2;
+        }else if($_GET['submit'] == 'หาร'){ $result = $n1 / $n2;
+        }
        ?>
         <div class="row p-1">
                 <div class="col-1">
@@ -46,7 +55,7 @@
                 </div>
                 <div class="col-2">
                     <input type="text" class="form-control" 
-                    name="resule" value="<?php echo $n1+$n2; ?>"/>
+                    name="resule" value="<?php echo $result; ?>"/>
                 </div>
         </div>
 <?php
